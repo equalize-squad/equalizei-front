3b4d57b5f330a9f8ebb6dc310750974895f5c6fa
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-06-06 12:55:31 -0300

    chore(ui): Add angular2-material ui components

--- src/system-config.ts
+++ src/system-config.ts
@@ -1,13 +1,36 @@
 /***********************************************************************************************
  * User Configuration.
  **********************************************************************************************/
-/** Map relative paths to URLs. */
-const map: any = {
-};
+ /** Map relative paths to URLs. */
+ const map:any = {
+   '@angular2-material': 'vendor/@angular2-material'
+ };
 
 /** User packages configuration. */
-const packages: any = {
-};
+const packages: any = {};
+
+// put the names of any of your Material components here
+const materialPkgs:string[] = [
+  'core',
+  'button',
+  'card',
+  'checkbox',
+  'grid-list',
+  'icon',
+  'input',
+  'list',
+  'progress-bar',
+  'progress-circle',
+  'radio',
+  'sidenav',
+  'slide-toggle',
+  'tabs',
+  'toolbar'
+];
+
+materialPkgs.forEach((pkg) => {
+  packages[`@angular2-material/${pkg}`] = {main: `${pkg}.js`};
+});
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
