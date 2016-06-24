9089146d5d24214eb12e52485b92b87a69e9486e
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-06-24 14:53:33 -0300

    feat(authentication): Add the Authentication and Authorization Guard

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate,
+         Router,
+         ActivatedRouteSnapshot,
+         RouterStateSnapshot } from '@angular/router';
+import { SessionController } from 'angular2-devise-token-auth';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    // Not using but worth knowing about
+    next:  ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ) {
+    console.log(SessionController.userSignedIn());
+    if (SessionController.userSignedIn()) {
+      return true;
+    }
+
+    this.router.navigate(['/sign-in']);
+    return false;
+  }
+}
