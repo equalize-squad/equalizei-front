e4df8ee5f7d4668f9135cb766937c0a4b504eb2c
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2016-07-13 10:13:23 -0300

    refactor(message-component): Rename file `message-type.enum` to `message-type.type`
    The message type is a type and not an Enum. As such, the file was renamed to reflect its content properly.

--- src/app/shared/message/message.model.ts
+++ src/app/shared/message/message.model.ts
@@ -1,4 +1,4 @@
-import { MessageType } from './message-type.enum';
+import { MessageType } from './message-type.type';
 export class Message {
   constructor(public type?: MessageType, public title?: String, public description?: String) {}
 }
